d2b6222cc2ecc0fb0efd0e24be5d8672e7293be1
mihailgaberov <mihail.gaberov@gmail.com>
2020-04-17 10:28:51 +0300

    :rocker: Installed bootstrap-vue. Adding Controls UI component.

---
+++ client/src/components/Controls.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="controls">
+    <label>Choose <i>period.</i></label>
+    <b-form-datepicker v-model="value" :min="min" :max="max" locale="en"></b-form-datepicker>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Controls',
+  data() {
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    // 15th two months prior
+    const minDate = new Date(today);
+    minDate.setMonth(minDate.getMonth() - 2);
+    minDate.setDate(15);
+    // 15th in two months
+    const maxDate = new Date(today);
+    maxDate.setMonth(maxDate.getMonth() + 2);
+    maxDate.setDate(15);
+
+    return {
+      value: '',
+      min: minDate,
+      max: maxDate,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
