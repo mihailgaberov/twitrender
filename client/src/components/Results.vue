38b943fff2abefa00582b953f7e04b6afd80f206
mihailgaberov <mihail.gaberov@gmail.com>
2020-04-18 10:38:17 +0300

    :rocket: Set minimum height of the results area.

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="alert alert-success">From <strong>22.10.2017</strong> to <strong>16.06.2018</strong>
-    the word <q class="example-quote french-quote">javascript</q>" has been twitted
-    <strong>777</strong> times.
+  <div class="results">
+    <div class="alert alert-success">From <strong>22.10.2017</strong> to <strong>16.06.2018</strong>
+      the word <q class="example-quote french-quote">javascript</q>" has been twitted
+      <strong>777</strong> times.
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Results',
@@ -12,6 +13,10 @@ export default {
 </script>
 
 <style scoped>
+  .results {
+    min-height: 180px;
+  }
+
   .alert {
     margin-top: 1em;
     font-size: 1.3em;
