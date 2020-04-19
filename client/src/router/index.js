d25646bb7fa7c9ce6bf5084b82726d88b93d8b39
mihailgaberov <mihail.gaberov@gmail.com>
2020-04-19 11:57:46 +0300

    :racehorse: Removed Ping component.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Ping from '../components/Ping.vue';
 import Twitrender from '../components/Twitrender.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/ping',
-    name: 'Ping',
-    component: Ping,
-  },
   {
     path: '/',
     name: 'Twitrender',
