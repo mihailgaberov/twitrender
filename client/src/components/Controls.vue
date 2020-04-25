ded184fc22186415b47cd0fad52db7884a457916
mihailgaberov <mihail.gaberov@gmail.com>
2020-04-25 10:51:41 +0300

    :sparkles: Set correct min and max date in the datepickers options.

--- client/src/components/Controls.vue
+++ client/src/components/Controls.vue
@@ -42,14 +42,8 @@ export default {
   data() {
     const now = new Date();
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    // 15th two months prior
-    const minDate = new Date(today);
-    minDate.setMonth(minDate.getMonth() - 2);
-    minDate.setDate(15);
-    // 15th in two months
+    const minDate = new Date(2017, 4, 27);
     const maxDate = new Date(today);
-    maxDate.setMonth(maxDate.getMonth() + 2);
-    maxDate.setDate(15);
 
     return {
       value: '',
